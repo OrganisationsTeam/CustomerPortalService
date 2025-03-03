7093fd307d6c1dc8eac0f7863ab870202d134549
hendrikmolgaard <Moelgaard01@hotmail.com>
2025-03-03 11:38:20 +0100

    Shared added

---
+++ CustomerPortalService.Shared/PurchaseOrder.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomerPortalService.Shared;
+
+public class PurchaseOrder
+{
+    public string OrderNumber { get; set; }
+    public DateTime OrderDate { get; set; }
+    public Customer Customer { get; set; }
+    public List<OrderedItem> OrderedItem { get; set; }
+}
+
+public class Customer
+{
+    public int CustomerId { get; set; }
+    public string Name { get; set; }
+}
+
+public class OrderedItem
+{
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+}
